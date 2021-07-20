2f317c9d08b62f14cd43058db15c7f6af22de4d0
testlinda <pikapika8440@hotmail.com>
2021-07-20 12:27:42 +0800

    Fix inconsistent use of tabs and spaces in indentation

--- fill_in_qnaire.py
+++ fill_in_qnaire.py
@@ -76,8 +76,8 @@ class FillInQnaire:
             option_no3 = self.webdriver.find_elements_by_xpath("//*[text()[contains(.,'am willing')]]")
             for element in option_no3:
                 self.webdriver.execute_script("arguments[0].click();", element)
-				
-			option_no4 = self.webdriver.find_elements_by_xpath("//*[text()[contains(.,'Moderna vaccine only')]]")
+
+            option_no4 = self.webdriver.find_elements_by_xpath("//*[text()[contains(.,'Moderna vaccine only')]]")
             for element in option_no4:
                 self.webdriver.execute_script("arguments[0].click();", element)
 
